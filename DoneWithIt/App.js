8267ab11203c85df7823fd61e23f7fe03fc9ef49
snehabanerjee2017 <snehabanerjee2017@gmail.com>
2025-09-26 21:26:41 +0200

    flex-direction

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -8,18 +8,25 @@ export default function App() {
     <View style={{
       backgroundColor: "white",
       flex: 1,
+      flexDirection: "column-reverse",
     }}>
       <View style={{
         backgroundColor: "dodgerblue",
-        flex:2
+        width:100,
+        height:100,
+        // flex:1
       }}/>
       <View style={{
         backgroundColor: "gold",
-        flex:1
+        width:100,
+        height:100,
+        // flex:1
       }}/>
       <View style={{
         backgroundColor: "tomato",
-        flex:1
+        width:100,
+        height:100,
+        // flex:1
       }}/>
     </View>
     
