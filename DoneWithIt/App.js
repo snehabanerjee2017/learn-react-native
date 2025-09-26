1b9bd357fdad72f878e57353350e8b6a9f59b612
snehabanerjee2017 <snehabanerjee2017@gmail.com>
2025-09-26 23:54:03 +0200

    View Image Screen

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View} from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 
 export default function App() { 
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
 
 
---
+++ DoneWithIt/app/screens/ViewImageScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Image, StyleSheet, View} from 'react-native';
+
+function ViewImageScreen(props) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.closeIcon}></View>
+            <View style={styles.deleteIcon}></View>
+            <Image
+                styles={styles.image}
+                source={require("../assets/family_pic.png")}>
+            </Image>
+        </View>
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    deleteIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: "blue",
+        position: "absolute",
+        top: 40,
+        right: 30,
+        zIndex: 1,
+    },
+    closeIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: "#fc5c65",
+        position: "absolute",
+        top: 40,
+        left: 30,
+        zIndex: 1,
+    },
+    container: {
+        backgroundColor: "#000",
+        flex: 1,
+        alignItems: "center",
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain",
+    },
+})
+export default ViewImageScreen;
