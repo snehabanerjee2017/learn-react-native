94214ae153631c98432431f52d3f5ed856e43b48
snehabanerjee2017 <snehabanerjee2017@gmail.com>
2025-09-26 21:23:23 +0200

    flex

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,43 +1,30 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableHighlight, Button, Alert, Platform, Dimensions} from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
-import { use } from 'react';
+import { View} from 'react-native';
 
 
 export default function App() { 
-  let x = 1;
-  console.log(useDeviceOrientation());
-
-  let landscape = false;
-  if(useDeviceOrientation()==="landscape") landscape = true;
-  else landscape = false;
-
-  const handlePress = () => {
-    console.log("Text Clicked");
-  }
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container} edges={["top", "left", "right"]}>
-        <View
-          style={{
-            backgroundColor: "dodgerblue",
-            width: "100%",
-            height: landscape ? "100%" : "30%",
-        }}></View>   
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View style={{
+      backgroundColor: "white",
+      flex: 1,
+    }}>
+      <View style={{
+        backgroundColor: "dodgerblue",
+        flex:2
+      }}/>
+      <View style={{
+        backgroundColor: "gold",
+        flex:1
+      }}/>
+      <View style={{
+        backgroundColor: "tomato",
+        flex:1
+      }}/>
+    </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0, 
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
 
 // Physical Pixels = Density-independent Pixels(DIPs) * Scale Factor
