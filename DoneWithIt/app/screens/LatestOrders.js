dc462619f006e0df9e38872be383e0c5fe3ca590
snehabanerjee2017 <snehabanerjee2017@gmail.com>
2025-09-29 19:26:05 +0200

    multiple items

--- DoneWithIt/app/screens/LatestOrders.js
+++ DoneWithIt/app/screens/LatestOrders.js
@@ -1,23 +1,60 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions, FlatList, View, StatusBar, ScrollView } from "react-native";
 import { WebView } from "react-native-webview";
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
+const { width } = Dimensions.get("window");
 
-function LatestOrders(props) {
+// List of Instagram post URLs
+const posts = [
+  "https://www.instagram.com/reel/DPEdGEVDSJU/",
+  "https://www.instagram.com/reel/DO8vcdUDUrI/",
+  "https://www.instagram.com/reel/DOycYmiDY2_/",
+  "https://www.instagram.com/reel/DOq4H_fjR7X/",
+  "https://www.instagram.com/reel/DOgZQblDRBV/",
+];
+
+function LatestOrders() {
   return (
-    <WebView
-      source={{ uri: "https://www.instagram.com/reel/DPEdGEVDSJU/?utm_source=ig_embed&amp;utm_campaign=loading" }}
-      style={styles.webview}
-      startInLoadingState
-      javaScriptEnabled
-      domStorageEnabled
-    />
+    <SafeAreaProvider>
+                        <SafeAreaView style={styles.container} edges={['top']}>
+                            <ScrollView showsVerticalScrollIndicator={false}>
+                                <FlatList
+                                  data={posts}
+                                  keyExtractor={(item, index) => index.toString()}
+                                  numColumns={2}
+                                  renderItem={({ item }) => (
+                                    <View style={styles.item}>
+                                      <WebView
+                                        source={{ uri: item }}
+                                        style={styles.webview}
+                                        startInLoadingState
+                                        javaScriptEnabled
+                                        domStorageEnabled
+                                      />
+                                    </View>
+                                  )}
+                                />
+                            </ScrollView>
+                        </SafeAreaView>
+                    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      paddingTop: StatusBar.currentHeight,
+    },
+  item: {
+    flex: 1,
+    margin: 20,
+    height: 200, // Adjust height for better viewing
+  },
   webview: {
     flex: 1,
+    borderRadius: 10,
+    overflow: "hidden",
   },
 });
 
